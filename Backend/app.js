9c4d4c0df1a6f528f87ce517dd90064b386d65c7
luckytsori8@gmail.com <luckytsori8@gmail.com>
2025-12-06 11:36:52 +0300

    chore(backend): implement full backend functionality including routes, controllers, models, and database connection

--- Backend/app.js
+++ Backend/app.js
@@ -6,12 +6,13 @@ import authRoutes from "./routes/auth.route.js";
 import productRoute from "./routes/product.route.js";
 import bannerRoute from "./routes/banner.route.js";
 import usersRoute from "./routes/user.route.js";
+import orderRoute from "./routes/order.route.js";
 
 const app = express();
 
-// ========================
+
 // MIDDLEWARE
-// ========================
+
 
 // Parse JSON body
 app.use(express.json());
@@ -22,17 +23,15 @@ app.use(cors());
 // Parse cookies
 app.use(cookieParser());
 
-// ========================
 // ROUTES
-// ========================
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/products", productRoute);
 app.use("/api/v1/banners", bannerRoute);
 app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/orders", orderRoute);
 
-// ========================
+
 // ERROR HANDLING MIDDLEWARE
-// ========================
 app.use(notFound);
 app.use(errorHandler);
 
